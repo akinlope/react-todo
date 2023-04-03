2584bfc176ab994c6fdb3f51de62f995f9c6ae0e
akinlope <akinrimisitt@gmail.com>
2023-04-03 13:51:45 +0100

    [Fixed Error 404]

--- src/components/add-view/View.js
+++ src/components/add-view/View.js
@@ -20,6 +20,7 @@ const View = ({ isTodo, setReload}) => {
   return (
     <div className=" mt-14">
       {isTodo.map((item) => {
+        console.log(item.id);
         return (
           <div onClick={()=> myFunc(item.id)} key={item.id} className=" flex justify-center mt-2 mb-2 cursor-pointer">
             <p  className=" p-3 shadow-3xl rounded text-txtColor font-mainTxt text-2xl font-extrabold mt-3">
